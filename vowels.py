245e3e4e114c9256e550432bbdac4c44849e2e70
Akash J <58515715+Akash280899@users.noreply.github.com>
2019-12-18 09:47:14 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,17 +1,13 @@
 a=int(input())
-l=[]
-v=['a','e','i','o','u','A','E','I','O','U',]
 for i in range(a):
-    b=input().split(" ")
-    b=[str(x) for x in str(b)]
-    l.append(b)
-for i in l:
-    l=list(i)
-    print(l)
-    for i in range(0,len(l)):
-        if(l[i]>='A' and l[i]<='Z') or (l[i]>='a' and l[i]<='z'):
-            if l[i] in v:
-                l[i]=l[i].upper()
-            else:
-                l[i]=l[i].lower()
-    print(''.join(l))
+    b=input()
+    c=[str(x) for x in str(b)]
+    e=[]
+    for j in range(len(c)):
+        if(c[j]=='a' or c[j]=='e' or c[j]=='i' or c[j]=='o' or c[j]=='u'):
+            e.append(c[j].upper())
+        else:
+            e.append(c[j].lower())
+    for i in e:
+        print(i,end="")
+    print()
