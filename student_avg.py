c3d9e148780b4e113dd499916c2bd9eeefcbbdca
Akash J <58515715+Akash280899@users.noreply.github.com>
2020-01-04 21:54:10 +0530

    Delete student_avg.py

--- student_avg.py
+++
@@ -1,4 +0,0 @@
-from collections import namedtuple
-n,arrangement = int(input()),input()
-Student = namedtuple('Student',arrangement)
-print(sum( [int(Student._make(input().split()).MARKS) for _ in range(n)])/n)
