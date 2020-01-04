85d2bb6c3ef0a4eb7fc7c15f79028ebb58b4b7c2
Akash J <58515715+Akash280899@users.noreply.github.com>
2020-01-04 21:46:43 +0530

    Update rep_words(ordered).py

--- Dictionary/rep_words(ordered).py
+++ Dictionary/rep_words(ordered).py
@@ -1,10 +1,10 @@
 #repeated words and its occurences
 from collections import OrderedDict
-words = OrderedDict()
-for _ in range(int(input())):
-    word = input()
-    words.setdefault(word, 0)+1
-    #words[word] += 1
+d = OrderedDict()
+for i in range(int(input())):
+    a = input()
+    d.setdefault(a, 0)
+    d[a] += 1
    
-print(len(words))
-print(*words.values())
+print(len(d))
+print(*d.values())
