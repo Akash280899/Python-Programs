2f24dad289a0e0d83ca3f5ea9c64b4422bb86592
Akash J <58515715+Akash280899@users.noreply.github.com>
2020-02-06 21:09:22 +0530

    Update bike_oops.py

--- bike_oops.py
+++ bike_oops.py
@@ -1,12 +1,12 @@
 class bike:
-    def __init__(self,vin,brand,model,engine,brake,cost):
+    def set_entry(self,vin,brand,model,engine,brake,cost):
         self.a=vin
         self.b=brand
         self.c=model
         self.d=engine
         self.e=brake
         self.f=cost
-    def display(self):
+    def get_entry(self):
         print("VIN:{}".format(self.a))
         print("Brand:{}".format(self.b))
         print("Model:{}".format(self.c))
@@ -16,14 +16,19 @@ class bike:
         print("")
     def __eq__(self, other):
         if isinstance(other, bike):
-            return self.a==other.a and self.b==other.b and self.c==other.c and self.d==other.d and self.e==other.e and self.f==other.f
-        return False
+            if self.a==other.a and self.b==other.b and self.c==other.c and self.d==other.d and self.e==other.e and self.f==other.f:
+                print("same")
+            else:
+                print("differ")
+        #return None
 print("Bike1 details")
-a=bike('MD2GJ3214JR258416','KTM','RC','390cc','Disk',225000.0)
+a=bike()
+a.set_entry('MD2GJ3214JR258416','KTM','RC','390cc','Disk',225000.0)
 print("Bike2 details")
-b=bike('MD2GJ3214JR258416','BMW','RC','390cc','Disk',225000.0)
+b=bike()
+b.set_entry('MD2GJ3214JR258416','BMW','RC','390cc','Disk',225000.0)
 print("Bike1")
-a.display()
+a.get_entry()
 print("Bike2")
-b.display()
+b.get_entry()
 print(a==b)
