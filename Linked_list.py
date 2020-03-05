0f679e79e8042b6423dc9d7d9358d62aaacd8f16
Akash J <58515715+Akash280899@users.noreply.github.com>
2020-03-05 21:50:20 +0530

    Create Linked_list.py

---
+++ Linked_list.py
@@ -0,0 +1,41 @@
+class Node: 
+    def __init__(self, data): 
+        self.data = data  
+        self.next = None 
+class LinkedList: 
+    def __init__(self): 
+        self.head = None
+    def printList(self): 
+        temp = self.head 
+        while (temp): 
+            print(temp.data), 
+            temp = temp.next
+if __name__=='__main__': 
+    llist = LinkedList() 
+    while(1):
+        print("1.Insert\n2.Delete\n3.Exit")
+        n=int(input())
+        if n==1:
+            if llist.head==None:
+                llist.head=Node(int(input()))
+                llist.last = llist.head 
+            else:
+                llist.last.next=Node(int(input()))
+                llist.last=llist.last.next
+        if n==2:
+            n=int(input())
+            temp=llist.head
+            while(temp is not None):
+                if temp.data==n:
+                    llist.head=temp.next
+                    temp=None
+                    llist.printList()
+                else:
+                    temp=temp.next
+        if n==3:
+            llist.printList()
+            exit()
+    
+   
+
+    
